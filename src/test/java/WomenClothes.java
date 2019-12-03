d3eac9d8152cac1ba273e5cf17d06c7c69ca1992
Jekaterina <katjenok@gmail.com>
2019-12-03 16:44:14 +0200

    Final version

--- src/test/java/WomenClothes.java
+++ src/test/java/WomenClothes.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ public class WomenClothes {
     private final By WOMANCLICK = By.xpath(".//a[@title='Women']");
     private final By LEFTBOX = By.xpath(".//ul[@class='tree dynamized']");
     private final By MAINBOX = By.xpath(".//a[@class='subcategory-name']");
+    private final By MINIBOX = By.xpath(".//h2[@class='title_block']");
     private static final Logger LOGGER = LogManager.getLogger(WomenClothes.class);
 
     @Test
@@ -34,7 +37,8 @@ public class WomenClothes {
         WomanClick.click();
 
         LOGGER.info("Check department count and equals it 2?");
-        Thread.sleep(1000);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(LEFTBOX));
         List<WebElement> we = driver.findElements(LEFTBOX);
         String[] leftNames = we.get(0).getText().split("\n", 0);
         String leftFisrst = leftNames[0].toString().toLowerCase();
@@ -42,7 +46,7 @@ public class WomenClothes {
         Integer count = leftNames.length;
         Assertions.assertEquals(2, count);
 
-       List<WebElement> mainwe = driver.findElements(MAINBOX);
+        List<WebElement> mainwe = driver.findElements(MAINBOX);
         Integer maincount = mainwe.size();
 
         String mainFirst = driver.findElements(MAINBOX).get(0).getText().toLowerCase();
@@ -53,38 +57,24 @@ public class WomenClothes {
         Assertions.assertEquals(leftSecond, mainSecond, "Wrong second name!");
 
         LOGGER.info("Resize window to mobile view");
-        Dimension size = new Dimension(400, 702);
+        Dimension size = new Dimension(400, 1000);
         driver.manage().window().setSize(size);
         LOGGER.info("Check department count and compare titles");
+        Thread.sleep(1000);
+        driver.findElement(MINIBOX).click();
 
+        List<WebElement> wesmall = driver.findElements(LEFTBOX);
+        String[] smallNames = we.get(0).getText().split("\n", 0);
+        String smallFisrst = leftNames[0].toString().toLowerCase();
+        String smallSecond = leftNames[1].toString().toLowerCase();
+        Integer minicount = leftNames.length;
+        Assertions.assertEquals(count, minicount);
+        Assertions.assertEquals(mainFirst, smallFisrst, "Wrong mini name!");
+        Assertions.assertEquals(mainSecond, smallSecond, "Wrong mini name!");
 
-
-
-
-
-        for ( String str1 : leftNames   )
-        {
-            System.out.println("Web Elements:" + str1);
-            if(subcategoryExists(str1))
-            {
-                System.out.println("Subcategory " + str1 + " exists");
-            }
-            else
-                System.out.println("Subcategory " + str1 + " not found");
-        }
-
-//        System.out.println("Web Elements:" + strs[0]);
-        LOGGER.info("Check titles is equals in left column and in the main page");
-        LOGGER.info("Resize window to mobile view");
-        LOGGER.info("Check department count and compare titles");
         LOGGER.info("Close browser");
-
+        LOGGER.info("Test is ended");
+        driver.close();
     }
-
-    boolean subcategoryExists (String subCategoryName)
-    {
-        return true;
-    }
-
 }
 
